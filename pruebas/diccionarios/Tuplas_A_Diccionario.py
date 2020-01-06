fcca10b44d88b3658222225cbdfde2684f2aa6cb
Florencia-97 <florrr1997@gmail.com>
2020-01-05 21:19:16 -0300

    Arreglo ejs en tarea

--- pruebas/diccionarios/Tuplas_A_Diccionario.py
+++ pruebas/diccionarios/Tuplas_A_Diccionario.py
@@ -1,7 +1,16 @@
 # def lista_tuplas_a_dic(lista_tuplas):
 
-def pruebas():
-    assert lista_tuplas_a_dic([(1,2),(1,4),(2,7)]) == {1: [2, 4], 2: [7]}
+class TestStringMethods(unittest.TestCase):
+
+    def test_lista_vacia(self):
+        self.assertEqual(lista_tuplas_a_dic([]), {})
+
+    def test_claves_repetidos(self):
+        self.assertEqual(lista_tuplas_a_dic([(1,2),(1,4),(2,7)]), {1: [2, 4], 2: [7]})
+
+    def test_claves_no_se_repiten(self):
+        self.assertEqual(lista_tuplas_a_dic([(1,2),(5,4),(2,7)]), {1: [2], 5:[4], 2: [7]})
 
 
-pruebas()
+if __name__ == '__main__':
+    unittest.main()
--- pruebas/tarea/Norma_Vector.py
+++ pruebas/tarea/Norma_Vector.py
@@ -2,8 +2,11 @@
 
 class TestTarea1(unittest.TestCase):
 
-    def test_norma(self):
+    def test_norma_con_valor_negativo(self):
         self.assertAlmostEqual(norma(3,2,-4),  5.385164807134504)
+
+    def test_norma_punto_origen(self):
+        self.assertAlmostEqual(norma(0,0,0), 0)
 
 
 if __name__ == '__main__':
