3bd65e835e8d7a1ddbec959ed67ff0bff35e6e30
Florencia-97 <florrr1997@gmail.com>
2020-01-08 17:33:47 -0300

    Pruebas para cola y pila

--- pruebas/colas/Torre_De_Control.py
+++ pruebas/colas/Torre_De_Control.py
@@ -1,2 +1,17 @@
 # class TorreDeControl:
 
+import queue
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_torre_de_control_vacia(self):
+        torreDeControl = TorreDeControl()
+        self.assertEqual(torreDeControl.asignar_pista(), "no hay vuelos en espera")
+    
+    def test_torre_de_control_con_vuelos(self):
+        torreDeControl = TorreDeControl()
+        torreDeControl.nuevo_arribo("AR156")
+        self.assertEqual(torreDeControl.ver_estado(), "Vuelos esperando para aterrizar: AR156")
+
+if __name__ == '__main__':
+    unittest.main()
--- pruebas/pilas/Expresion_Balanceada.py
+++ pruebas/pilas/Expresion_Balanceada.py
@@ -1,1 +1,22 @@
 # def expresion_balanceada(exp):
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_expresion_balanceada_corta(self):
+        msg = "(x+y)/2 es una expresión balanceada"
+        self.assertTrue(expresion_balanceada('(x+y)/2'), msg)
+
+    def test_expresion_balanceada_larga(self):
+        msg = "'[8*4(x+y)]+{2/5}' es una expresión balanceada"
+        self.assertTrue(expresion_balanceada('[8*4(x+y)]+{2/5}'), msg)
+    
+    def test_expresion_no_balanceada_corta(self):
+        msg = "'(x+y]/2' no es una expresión balanceada"
+        self.assertFalse(expresion_balanceada('(x+y]/2'), msg)
+
+    def test_expresion_no_balanceada_larga(self):
+        msg = "'1+)2(+3' no es una expresión balanceada"
+        self.assertFalse(expresion_balanceada('1+)2(+3'), msg)
+
+if __name__ == '__main__':
+    unittest.main()
